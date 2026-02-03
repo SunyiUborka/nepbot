d3611b2f1252ecc48f46f957c5fe123b444b31e5
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2026-02-03 12:23:11 +0100

    update & refactor

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ import logger from './logger.js';
 import * as dotenv from 'dotenv';
 dotenv.config()
 
-const USER_AGENT =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
-  'AppleWebKit/537.36 (KHTML, like Gecko) ' +
-  'Chrome/139.0.0.0 Safari/537.36 Edg/139.0.0.0'
-
+const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0'
 const cookieJar = new tough.CookieJar();
+let logged = false
 
 if (process.env.SERVER_COOKIE)
   cookieJar.setCookie(
@@ -45,90 +42,62 @@ const loadData = () => {
 }
 loadData();
 
-const parseCookiesToObject = (cookiesArray) => {
-  const cookieMap = {};
-
-  for (const cookieHeader of cookiesArray) {
-    const cookieStr = String(cookieHeader);
-
-    let clean = cookieStr;
-    if (cookieStr.startsWith('Cookie="'))
-      clean = cookieStr.replace(/^Cookie="/, '').replace(/"$/, '');
-
-    const firstPart = clean.split(';')[0].trim();
-
-    const [name, ...valueParts] = firstPart.split('=');
-    const value = valueParts.join('=');
-
-    cookieMap[name] = value;
-  }
-
-  return cookieMap;
-}
-
-const fetchWithCookies = async (url) => {
+const login = async () => {
+  logger.info("Authenticating...")
   try {
-    const response = await client.post(url, {
+    const r = await client.post(`${process.env.BASE_URL}/hallgato/api/Account/Authenticate`, {
       username: process.env.CODE,
       password: process.env.PASSWORD
     });
-
-    const cookies = parseCookiesToObject(await cookieJar.getCookies(url));
-    const { data } = response.data
-
-    return {
-      data,
-      cookies: cookies
-    };
+      client.defaults.headers.common['Authorization'] = `Bearer ${r.data.data.accessToken}`
+      if (r.status == 200) {
+          logger.info(`Authentication success`)
+          logged = true
+      }else logger.error(`Error during Authentication: ${r.statusText}`)
   } catch (e) {
     logger.error(`Error during authentication: ${e.message}`);
   }
 }
 
 const getCourses = async () => {
+	logger.info(`Fetching courses...`)
   try {
-    const data = await fetchWithCookies(`${process.env.BASE_URL}/hallgato/api/Account/Authenticate`)
-    const cookieHeader = Object.entries(data.cookies).map(([key, value]) => `${key}=${value}`).join('; ');
-
-    return (await client.get(`${process.env.BASE_URL}/hallgato/api/SubjectApplication/GetScheduledCourses?model.termId=70632`, {
-      headers: {
-        Authorization: `Bearer ${data.data.accessToken}`,
-        Cookie: cookieHeader
-      }
-    })).data
+    const schedules = (await client.get(`${process.env.BASE_URL}/hallgato/api/SubjectApplication/GetScheduledCourses?request.termId=70633`))
+        .data
+    logger.info(`Found ${schedules.length} courses`)
+    return schedules
   } catch (e) {
+    console.log(e)
     logger.error(`Error during fetching courses: ${e.message}`);
   }
-
 }
 
 const sendMessage = async (content) => {
+  const dcClient = wrapper(
+    axios.create({
+        withCredentials: true,
+        headers: {
+          Authorization: `Bot ${process.env.DC_BOT_TOKEN}`
+        }
+    })
+  );
   const url = 'https://discord.com/api'
-
-  const channelId = (await axios.post("https://discord.com/api/v10/users/@me/channels", {
+  const channelId = await (await dcClient.post("https://discord.com/api/v10/users/@me/channels", {
     recipient_id: process.env.DC_USER_ID
-  }, {
-    headers: {
-      Authorization: `Bot ${process.env.DC_BOT_TOKEN}`,
-    }
   })).data.id
 
   try {
-    axios.post(`${url}/channels/${channelId}/messages`, {
+    await dcClient.post(`${url}/channels/${channelId}/messages`, {
       content: content
-    }, {
-      headers: {
-        Authorization: `Bot ${process.env.DC_BOT_TOKEN}`,
-      }
     })
   } catch (e) {
+    console.log(e)
     logger.error(`Error during sending message: ${e.message}`);
   }
-
 }
 
+
 const checkCourses = async () => {
-  const now = new Date();
   logger.info('Checking courses... ');
 
   try {
@@ -160,9 +129,13 @@ const checkCourses = async () => {
     logger.error(`Error during checking courses: ${e.message}`);
   }
 }
-try {
-  sendMessage("NepBot started!");
-  checkCourses();
-} catch (e) {
-  logger.error(`Error during initial setup: ${e.message}`);
-}
+
+(async () => {
+  try {
+    await sendMessage("NepBot started!");
+      await login()
+      if (logged) checkCourses();
+  } catch (e) {
+    logger.error(`Error during initial setup: ${e.message}`);
+  }
+})()
